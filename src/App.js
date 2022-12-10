b65a17bca4137c303a11a8caf0543b88600f3eca
Thashfeeq jaseel <hashjhn4820@gmail.com>
2022-12-10 19:01:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css"
+
+
 
 function App() {
+
+const [toDo,setToDo]=useState('')
+const [toDos,setToDos]=useState([])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input  onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(),delete:false, text:toDo, status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+      {
+        toDos.map((obj)=>{
+
+          if(obj.delete===!true){
+            return(
+            
+              <div className="todo">
+  
+                <div className="left">
+                  <input onChange={(e)=>{
+                    console.log(e);
+                    console.log(obj);
+                    setToDos(toDos.filter(obj2=>{
+                      console.log(obj2);
+                      if (obj2.id===obj.id){
+                        obj2.status=e.target.checked
+                      }
+                      return obj2
+  
+                    }))
+                  }}  type="checkbox" name="" id="" />
+                  
+                  <p>{obj.text}</p>
+                </div>
+  
+  
+                <div className="right">
+                  <i onClick={()=>setToDos(toDos.filter(obj3=>{
+                    if(obj3.id===obj.id){
+                      obj.delete=true
+                      console.log(obj3);
+                    }
+                    return obj3
+  
+                  }))}  className="fas fa-times"></i>
+                </div>
+              </div>
+           
+            )
+          }
+
+          
+         
+        })
+        
+      }
+
+      {
+        toDos.map((obj)=>{
+
+          if(obj.delete===false){
+            if(obj.status){
+              return(<h1>{obj.text}</h1>)
+            }
+          }
+          
+          return null;
+        })
+      }
+       </div>
+      
     </div>
   );
 }
 
-export default App;
+export default App
